693bc7ffb5bdaad63dc93b0053219da6f78d23c3
ztwu <123>
2018-06-04 08:59:13 +0800

    优化

--- src/main/java/com/iflytek/edu/utils/MyGroupingComparator.java
+++ src/main/java/com/iflytek/edu/utils/MyGroupingComparator.java
@@ -3,6 +3,8 @@ package com.iflytek.edu.utils;
 import org.apache.hadoop.io.RawComparator;
 import org.apache.hadoop.io.WritableComparator;
 
+import java.util.Arrays;
+
 /**
  * Created with Intellij IDEA.
  * User: ztwu2
@@ -33,9 +35,15 @@ public class MyGroupingComparator implements RawComparator<MyKeyPair> {
      * @param l2 表示第二个参与比较的字节数组的偏移量
      */
     ////基于字节排序，不需要序列化和反序列化
-    public int compare(byte[] b1, int s1, int i1, byte[] b2, int s2, int i3) {
+    //比较的时候，从开始的位置(s1和s2)，并且长度是(l1和l2)
+    //允许实现它来在字节流的层面比较，而不用反序列化为对象，因此，可以避免过多的对象的创建。
+    public int compare(byte[] b1, int s1, int i1,
+                       byte[] b2, int s2, int i2) {
         System.out.println("Group == RawComparator中的compare()方法是基于字节的比较");
-        return WritableComparator.compareBytes(b1,s1,8, b2,s2,8);
+        System.out.println("Group b1 == "+ Arrays.toString(b1)+"="+s1+"="+i1);
+        System.out.println("Group b2 == "+Arrays.toString(b1)+"="+s2+"="+i2);
+        return WritableComparator.compareBytes(b1,s1,i1,
+                b2,s2,i2);
     }
 
     //java 两种比较器
